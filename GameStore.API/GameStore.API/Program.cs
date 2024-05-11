729bae931ff6aa1f62bb5f42e6e8eaf6dbffbfad
Zen Le <44715845+zenkiet@users.noreply.github.com>
2024-05-11 17:58:31 +0700

    Adding the Game Entity

--- GameStore.API/GameStore.API/Program.cs
+++ GameStore.API/GameStore.API/Program.cs
@@ -1,3 +1,5 @@
+using GameStore.API.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,45 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+List<Game> games =
+[
+    new Game()
+    {
+        Id = 1,
+        Name = "The Legend of Zelda: Breath of the Wild",
+        Genre = "Action-adventure",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    },
+
+    new Game()
+    {
+        Id = 2,
+        Name = "Super Mario Odyssey",
+        Genre = "Platformer",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    },
+
+    new Game()
+    {
+        Id = 3,
+        Name = "Mario Kart 8 Deluxe",
+        Genre = "Racing",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    },
+
+    new Game()
+    {
+        Id = 4,
+        Name = "Splatoon 2",
+        Genre = "Shooter",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    }
+];
 
 var summaries = new[]
 {
