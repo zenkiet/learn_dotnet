db441ffb9867ee967eb78b5f738d0f40fd7d7bf8
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2024-05-22 22:32:33 +0700

    Adding the Game Entity

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,5 +1,46 @@
+using GameStore.API.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<Game> games =
+[
+    new Game()
+    {
+        Id = 1,
+        Name = "The Legend of Zelda: Breath of the Wild",
+        Genre = "Action-adventure",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    },
+
+    new Game()
+    {
+        Id = 2,
+        Name = "Super Mario Odyssey",
+        Genre = "Platformer",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    },
+
+    new Game()
+    {
+        Id = 3,
+        Name = "Mario Kart 8 Deluxe",
+        Genre = "Racing",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    },
+
+    new Game()
+    {
+        Id = 4,
+        Name = "Splatoon 2",
+        Genre = "Shooter",
+        Price = 59.99m,
+        ImageUri = "https://placeholder.co/300",
+    }
+];
 
 app.MapGet("/", () => "Hello World!");
 
