ebbd73a597ce9db61653b2bf24b7948ad805aa7b
Zen Le <44715845+zenkiet@users.noreply.github.com>
2024-06-01 15:40:57 +0700

    Adding the Games Repository

--- GameStore.API/GameStore.API/Endpoints/GameEndpoint.cs
+++ GameStore.API/GameStore.API/Endpoints/GameEndpoint.cs
@@ -1,96 +1,52 @@
 using GameStore.API.Entities;
+using GameStore.API.Repositories;
 
 namespace GameStore.API.Endpoints;
 
 public static class GameEndpoint
 {
-    static List<Game> games =
-    [
-        new Game()
-        {
-            Id = 1,
-            Name = "The Legend of Zelda: Breath of the Wild",
-            Genre = "Action-adventure",
-            Price = 59.99m,
-            ImageUri = "https://placeholder.co/300",
-        },
-
-        new Game()
-        {
-            Id = 2,
-            Name = "Super Mario Odyssey",
-            Genre = "Platformer",
-            Price = 59.99m,
-            ImageUri = "https://placeholder.co/300",
-        },
-
-        new Game()
-        {
-            Id = 3,
-            Name = "Mario Kart 8 Deluxe",
-            Genre = "Racing",
-            Price = 59.99m,
-            ImageUri = "https://placeholder.co/300",
-        },
-
-        new Game()
-        {
-            Id = 4,
-            Name = "Splatoon 2",
-            Genre = "Shooter",
-            Price = 59.99m,
-            ImageUri = "https://placeholder.co/300",
-        }
-    ];
-
+    private static readonly InMemGamesRepository Repository = new InMemGamesRepository();
+     
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
         var gameGroup = routes.MapGroup("/games").WithParameterValidation();    
 
-        gameGroup.MapGet("/", () => games)
+        gameGroup.MapGet("/", () => Repository.GetAll())
             .WithName("GetGames").WithOpenApi();
 
         gameGroup.MapGet("/{id}", (int id) =>
         {
-            var game = games.FirstOrDefault(g => g.Id == id);
-            return game is null ? Results.NotFound() : Results.Ok(game);
+            var game = Repository.Get(id);
+            return game is not null ? Results.Ok(game) : Results.NotFound();
         }).WithName("GetGameById").WithOpenApi();
 
         gameGroup.MapPost("/", (Game game) =>
         {
-            game.Id = games.Max(g => g.Id) + 1;
-            games.Add(game);
+            Repository.Add(game);
             return Results.CreatedAtRoute("GetGameById", new { id = game.Id }, game);
         }).WithName("CreateGame").WithOpenApi();
 
         gameGroup.MapPut("/{id}", (int id, Game game) =>
         {
-            var existingGame = games.FirstOrDefault(g => g.Id == id);
-            if (existingGame is null)
-            {
-                return Results.NotFound();
-            }
-
-            existingGame.Name = game.Name;
-            existingGame.Genre = game.Genre;
-            existingGame.Price = game.Price;
-            existingGame.ImageUri = game.ImageUri;
-            existingGame.UpdatedAt = DateTime.Now;
-
+            var existingGame = Repository.Get(id);
+            
+            if (existingGame is null) return Results.NotFound();
+            
+            Repository.Update(game);
             return Results.NoContent();
+
         }).WithName("UpdateGame").WithOpenApi();
 
         gameGroup.MapDelete("/{id}", (int id) =>
         {
-            Game? existingGame = games.FirstOrDefault(g => g.Id == id);
-    
-            if (existingGame is null)
-            {
-                return Results.NotFound();
-            }
+            var existingGame = Repository.Get(id);
 
-            games.Remove(existingGame);   
-            return Results.NoContent();
+            if (existingGame is not null)
+            {
+                Repository.Delete(id);  
+                return Results.NoContent();
+            }
+            return Results.NotFound();
         }).WithName("DeleteGame").WithOpenApi();
 
         return gameGroup;
